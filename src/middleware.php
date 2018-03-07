b1587be55eecde713591b7b285b5efaa1ed5b430
KierenRhys <yann.lemoalsio@gmail.com>
2018-03-07 09:06:06 +0100

    Update middleware.php

--- src/middleware.php
+++ src/middleware.php
@@ -4,10 +4,11 @@ $app->options('/{routes:.+}', function ($request, $response, $args) {
     return $response;
 });
 
+// All origin allowed to request API
 $app->add(function ($req, $res, $next) {
     $response = $next($req, $res);
     return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Origin', '*') // Allow all origin -> *
             ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
