79f614b814d5d2ac9d83bb2d7bcd580744aecd9d
KierenRhys <yann.lemoalsio@gmail.com>
2018-03-07 09:08:33 +0100

    Update actus.php

--- src/components/actus.php
+++ src/components/actus.php
@@ -6,8 +6,8 @@ use Slim\Http\Response;
 // GET
 // Liste des 5 dernières actus (http://127.0.0.1/php_ecran_rest_api/public/actus)
 $app->get('/actus', function (Request $request, Response $response, array $args) {
-    $sth = $this->db->prepare("SELECT * FROM actualite ORDER BY date_ajout_actualite DESC LIMIT 5");
-    $sth->execute();
+    $sth = $this->db->prepare("SELECT * FROM actualite ORDER BY date_ajout_actualite DESC LIMIT 5"); // Préparation de la requête
+    $sth->execute(); // Exécution de la requête
     $actus = $sth->fetchAll();
     return $this->response->withJson($actus);
 });
@@ -18,12 +18,13 @@ $app->post('/nouvelleActu', function ($request, $response) {
     $input = $request->getParsedBody();
     $sql = "INSERT INTO `actualite`(`titre_actualite`, `contenu_actualite`, `date_ajout_actualite`, `image_url_actualite`) 
             VALUES (:titre_actualite, :contenu_actualite, :date_ajout_actualite, :image_url_actualite);";
-    $sth = $this->db->prepare($sql);
+    $sth = $this->db->prepare($sql); // Préparation de la requête
+    // Bindings de la requête
     $sth->bindParam("titre_actualite", $input['titre_actualite']);
     $sth->bindParam("contenu_actualite", $input['contenu_actualite']);
     $sth->bindParam("date_ajout_actualite", date('Y-m-d H:i:s'));
     $sth->bindParam("image_url_actualite", $input['image_url_actualite']);
-    $sth->execute();
+    $sth->execute(); // Exécution de la requête
     return $this->response->withJson($input);
 });
 
@@ -35,20 +36,22 @@ $app->put('/majActu/[{id}]', function ($request, $response, $args) {
                                     `contenu_actualite`=:contenu_actualite,
                                     `image_url_actualite`=:image_url_actualite
                                 WHERE `id_actualite`=:id";
-    $sth = $this->db->prepare($sql);
+    $sth = $this->db->prepare($sql); // Préparation de la requête
+    // Bindings de la requête
     $sth->bindParam("id", $args['id']);
     $sth->bindParam("titre_actualite", $input['titre_actualite']);
     $sth->bindParam("contenu_actualite", $input['contenu_actualite']);
     $sth->bindParam("image_url_actualite", $input['image_url_actualite']);
-    $sth->execute();
+    $sth->execute(); // Exécution de la requête
     return $this->response->withJson($input);
 });
 
 // DELETE
 // Suppression d'une actualite (http://127.0.0.1/php_ecran_rest_api/public/suppActu/13)
 $app->delete('/suppActu/[{id}]', function ($request, $response, $args) {
-    $sth = $this->db->prepare("DELETE FROM `actualite` WHERE `id_actualite`=:id");
+    $sth = $this->db->prepare("DELETE FROM `actualite` WHERE `id_actualite`=:id"); // Préparation de la requête
+    // Bindings de la requête
     $sth->bindParam("id", $args['id']);
-    $sth->execute();
+    $sth->execute(); // Exécution de la requête
     return $this->response->withJson($input);
 });
